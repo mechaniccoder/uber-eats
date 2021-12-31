057334ffab9f8ea974c0fbd049cf65c453a421f6
mechaniccoder <yuseunghwan94@gmail.com>
2021-12-31 18:59:57 +0900

    feat: connect mongodb

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
+import { MongooseModule } from '@nestjs/mongoose'
 import { join } from 'path'
 import { RestaurantModule } from './restaurant/restaurant.module'
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017', {
+      dbName: 'uber',
+      authSource: 'uber',
+      family: 4,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
