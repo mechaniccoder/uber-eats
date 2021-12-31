bc586ec6de03a3bdeace25ffb30fad5ccaa8b3df
mechaniccoder <yuseunghwan94@gmail.com>
2021-12-31 19:33:21 +0900

    feat: validate env variables using joi

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,9 @@
+import { join } from 'path'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
 import { MongooseModule } from '@nestjs/mongoose'
-import { join } from 'path'
+import * as Joi from 'joi'
 import { RestaurantModule } from './restaurant/restaurant.module'
 
 @Module({
@@ -15,6 +16,11 @@ import { RestaurantModule } from './restaurant/restaurant.module'
           : process.env.NODE_ENV === 'prod'
           ? '.env.prod'
           : '.env.test',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test'),
+        DB_URL: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
     MongooseModule.forRoot(process.env.DB_URL, {
       dbName: process.env.DB_NAME,
