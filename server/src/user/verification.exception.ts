23a6c0897bb4c298194b66f89e7c3c047e2fa9e0
mechaniccoder <yuseunghwan94@gmail.com>
2022-02-09 00:01:15 +0900

    feat: throw error when code is verified

--- server/src/user/verification.exception.ts
+++ server/src/user/verification.exception.ts
@@ -5,3 +5,9 @@ export class CodeNotMatchException extends HttpException {
     super('Code not match', HttpStatus.BAD_REQUEST)
   }
 }
+
+export class CodeAlreadyVerifiedException extends HttpException {
+  constructor() {
+    super('Code already verified', HttpStatus.BAD_REQUEST)
+  }
+}
--- server/src/user/verification.service.ts
+++ server/src/user/verification.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { VerifyCodeDto } from './dto/verify-code.dto'
 import { User, UserModel, UserWithoutPassword } from './schema/user.schema'
-import { CodeNotMatchException } from './verification.exception'
+import { CodeAlreadyVerifiedException, CodeNotMatchException } from './verification.exception'
 import { InjectModel } from '@nestjs/mongoose'
 
 @Injectable()
@@ -9,6 +9,8 @@ export class VerificationService {
   constructor(@InjectModel(User.name) private readonly userModel: UserModel) {}
 
   async verify(user: UserWithoutPassword, verifyCodeDto: VerifyCodeDto) {
+    if (user.verification.isVerified) throw new CodeAlreadyVerifiedException()
+
     if (user.verification.code !== verifyCodeDto.code) throw new CodeNotMatchException()
 
     await this.userModel.findByIdAndUpdate(user.id, {
