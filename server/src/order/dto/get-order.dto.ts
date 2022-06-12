fe12f2dfdb3d85381feb0a7265b61115b33fefaa
mechaniccoder <yuseunghwan94@gmail.com>
2022-06-12 18:02:09 +0900

    feat: change to nullable input

--- server/src/order/dto/get-order.dto.ts
+++ server/src/order/dto/get-order.dto.ts
@@ -1,9 +1,12 @@
-import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql'
-import { Order } from '../order.schema'
+import { Field, InputType, ObjectType } from '@nestjs/graphql'
+import { Order, OrderStatus } from '../order.schema'
 import { ResponseDto } from '../../shared/dto/response.dto'
 
 @InputType()
-export class GetOrderInput extends PickType(Order, ['status'], InputType) {}
+export class GetOrderInput {
+  @Field((type) => OrderStatus, { nullable: true })
+  status?: OrderStatus
+}
 
 @ObjectType()
 export class GetOrderRes extends ResponseDto {
