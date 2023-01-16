05a80d50f55b2babc6e044d6c69010cd30acace4
mechaniccoder <yuseunghwan94@gmail.com>
2023-01-16 23:31:29 +0900

    feat: sign up page

--- client/app/signin/page.tsx
+++ client/app/signin/page.tsx
@@ -1,35 +1,40 @@
 export default function SignInPage() {
   return (
-    <div className="justify-cent items-center">
-      <div className="mx-auto flex max-w-[360px] flex-col ">
+    <div className="justify-cent items-center bg-gray-800">
+      <div className="mx-auto flex max-w-[360px] flex-col gap-3 rounded-lg bg-white p-6">
         <h1>전화번호나 이메일이 어떻게 되시나요?</h1>
         {/* sign in form */}
         <form className="flex flex-col gap-3">
           <input
-            className="min-h-[3rem] rounded-lg bg-gray-200 px-3 py-[0.625rem]"
+            className="min-h-[3rem] rounded-lg bg-gray-200 px-3 py-[0.625rem] focus:outline-none focus:ring-2"
             type="text"
             autoComplete="email"
             autoCorrect="off"
             autoCapitalize="off"
             spellCheck="false"
             autoFocus
-            placeholder="Enter your email or phone number"
-            aria-label="Enter your email or phone number"
+            placeholder="Enter your email"
+            aria-label="Enter your email"
           />
-          <button className="btn-primary bg-black text-white">Continue</button>
+
+          <input
+            className="min-h-[3rem] rounded-lg bg-gray-200 px-3 py-[0.625rem] focus:outline-none focus:ring-2"
+            type="text"
+            autoComplete="email"
+            autoCorrect="off"
+            autoCapitalize="off"
+            spellCheck="false"
+            autoFocus
+            placeholder="Enter your password"
+            aria-label="Enter your password"
+          />
+          <button
+            type="submit"
+            className="btn-primary bg-black text-white ring-sky-400 focus:outline-none focus:ring-2"
+          >
+            Continue
+          </button>
         </form>
-
-        {/* divider */}
-        <div className="flex items-center before:h-px before:flex-1 before:bg-gray-400 after:h-px after:flex-1 after:bg-gray-400">
-          <p className="mx-2">or</p>
-        </div>
-
-        {/* social login */}
-        <div className="flex flex-col gap-2">
-          <button className="btn-primary bg-gray-200 text-white">Continue with Google</button>
-          <button className="btn-primary bg-gray-200 text-white">Continue with Facebook</button>
-          <button className="btn-primary bg-gray-200 text-white">Continue with Apple</button>
-        </div>
       </div>
     </div>
   )
