47eeeb198585c474b124e6c101ff968b34f55b5f
mechaniccoder <yuseunghwan94@gmail.com>
2023-02-02 00:07:40 +0900

    config: set up absolute path in test

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -3,6 +3,10 @@ import type { Config } from 'jest'
 const config: Config = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '@/(.*)': '<rootDir>/src/$1',
+  },
 }
 
 export default config
